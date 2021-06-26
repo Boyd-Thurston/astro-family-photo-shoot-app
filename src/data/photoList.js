535c7648d28d8e0f160cc48093289bf31062825a
Boyd_Thurston <boydthurston@gmail.com>
2021-06-26 13:46:52 +1200

    incorperate data structer

---
+++ src/data/photoList.js
@@ -0,0 +1,64 @@
+/*
+basic data structure for the project
+
+expected as an arry of objects with the following properties:
+{
+  id: number,
+  tags: array[ ...names:string ]
+  imageUrl: string
+}
+*/
+
+
+export default photoList = [
+  {
+    id: 001,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 002,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 003,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 004,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 005,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 006,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 007,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 008,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 009,
+    tags: [],
+    imageUrl: ''
+  },
+  {
+    id: 010,
+    tags: [],
+    imageUrl: ''
+  },
+]
